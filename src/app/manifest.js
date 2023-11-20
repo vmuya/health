fe81fdbf55900838b1ecd34e090ca9306a7182c4
vmuya <victor@archis.company>
2023-11-20 19:28:11 +0300

    Update manifeest

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,33 +1,34 @@
 export default function manifest() {
-    return {
-      name: 'Next.js App',
-      short_name: 'Next.js App',
-      description: 'Next.js App',
-      start_url: '/',
-      display: 'standalone',
+  return {
+    name: "Afya Health Blog - Health and Fitness Tips for busy people",
+    short_name: "Afya Health Blog",
+    description:
+      "Discover a treasure trove of expert insights on health, fitness, and well-being. Explore our latest articles covering nutrition, mental health, exercise, and more. Take a step towards a healthier you with our health and wellness blog.",
+    start_url: "/",
+    display: "standalone",
     //   background_color: '#fff',
     //   theme_color: '#fff',
-      icons: [
-        {
-          src: '/favicon-32x32.png',
-          sizes: '32x32',
-          type: 'image/png',
-        },
-          {
-          src: '/favicon-16x16.png',
-          sizes: '16x16',
-          type: 'image/png',
-        },
-            {
-          src: '/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-            {
-          src: '/android-chrome-512x512',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    }
-  }
+    icons: [
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
